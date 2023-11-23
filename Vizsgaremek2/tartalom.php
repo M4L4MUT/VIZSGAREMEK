67cc7e77a7a818081cc4f968cb49a394807d519d
M4L4MUT <113418446+M4L4MUT@users.noreply.github.com>
2023-11-23 09:50:06 +0100

    Add files via upload

---
+++ Vizsgaremek2/tartalom.php
@@ -0,0 +1,17 @@
+<?php
+
+switch ($menu) {
+    case 'főoldal':
+        require_once './pages/fooldal.php';
+        break;
+    case 'kapcsolat':
+        require_once './pages/kapcsolat.php';
+        break;
+    case 'kivalasztottauto':
+        require_once './pages/kivalasztottauto.php';
+        break;
+    default:
+        require_once './pages/fooldal.php';
+        break;
+}
+
